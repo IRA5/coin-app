68d46fcd3e1d0eb59fda6e1ebc789f69937bafab
Ali <ira5@njit.edu>
2021-10-05 10:46:57 -0400

    Initial Commit

---
+++ coin app/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  coin app
+//
+//  Created by Irtaza Ali on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    var ImageList = [#imageLiteral(resourceName: "Heads"),#imageLiteral(resourceName: "Tails")]
+    
+ 
+    
+    @IBAction func click(_ sender: Any) {
+        let randomPosition = Int.random(in: 0...1)
+        //who.what = image
+        
+        image.image = ImageList[randomPosition]
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
+
+    
+
+
+
